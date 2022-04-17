a0bdd4d999e2da8efec64b42d5963cbad853e37d
Borcea <cristian.borcea@sap.com>
2022-04-17 17:26:11 +0300

    added UI deployer, main views and related controls

--- sql-scripts/createTable.sql
+++ sql-scripts/createTable.sql
@@ -17,3 +17,17 @@ CREATE TABLE "Booking" (
 PRIMARY KEY (ID),
 FOREIGN KEY ("Passenger_ID") REFERENCES "Passenger" ON DELETE CASCADE
 );
+
+INSERT INTO "Passenger" VALUES('1','Ion', 'Popescu', 'popescu.ion@gmail.com', '0738111111');
+INSERT INTO "Passenger" VALUES('2','Cristian', 'Borcea', 'borcea.cristian@gmail.com', '0738222222');
+INSERT INTO "Passenger" VALUES('3','Gheorghe', 'Bobonete', 'gheorghe.bobonete@gmail.com', '0738333333');
+INSERT INTO "Passenger" VALUES('4','Dumitru', 'Mutu', 'dumitru.mutu@gmail.com', '0738444444');
+INSERT INTO "Passenger" VALUES('5','Anca', 'Popovici', 'anca.popovici@gmail.com', '0738555555');
+INSERT INTO "Passenger" VALUES('6','Elena', 'Diaconescu', 'elene.diaconescu@gmail.com', '0738666666');
+
+INSERT INTO "Booking" VALUES('1','active', '2022-07-12T12:00', '2022-07-12T12:00', 'London', '1');
+INSERT INTO "Booking" VALUES('2','active', '2022-08-12T12:00', '2022-08-12T12:00', 'Paris', '2');
+INSERT INTO "Booking" VALUES('3','active', '2022-09-12T12:00', '2022-09-12T12:00', 'Berlin', '3');
+INSERT INTO "Booking" VALUES('4','active', '2022-10-12T12:00', '2022-10-12T12:00', 'Amsterdam', '4');
+INSERT INTO "Booking" VALUES('5','active', '2022-11-12T12:00', '2022-11-12T12:00', 'Sofia', '5');
+INSERT INTO "Booking" VALUES('6','active', '2022-12-12T12:00', '2022-12-12T12:00', 'Prague', '6');
